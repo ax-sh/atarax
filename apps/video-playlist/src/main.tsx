cc6568a8bcec2d89f3a224baa8062d0d5914c47e
Axm <56112475+ax-sh@users.noreply.github.com>
2022-01-28 14:30:22 +0545

    add react query

--- apps/video-playlist/src/main.tsx
+++ apps/video-playlist/src/main.tsx
@@ -3,14 +3,18 @@ import * as ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import 'virtual:windi.css';
 import 'virtual:windi-devtools';
-
+import { QueryClient, QueryClientProvider } from 'react-query';
 import App from './app/App';
+
+const queryClient = new QueryClient();
 
 ReactDOM.render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </QueryClientProvider>
   </StrictMode>,
   document.getElementById('root')
 );
