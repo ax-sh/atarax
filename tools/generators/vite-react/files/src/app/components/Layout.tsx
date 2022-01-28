42154bde63fb9b0b3eb9da2848fbda3d060d710a
Axm <56112475+ax-sh@users.noreply.github.com>
2022-01-28 12:46:40 +0545

    add layout

--- tools/generators/vite-react/files/src/app/components/Layout.tsx
+++ tools/generators/vite-react/files/src/app/components/Layout.tsx
@@ -1,8 +1,13 @@
-function Layout({ children, ...props }: React.PropsWithChildren<{}>) {
-  return (
-    <div role={'Layout'} {...props}>
-      {children}
-    </div>
-  );
+function Layout({ children }) {
+  return <div>{children}</div>;
 }
 export default Layout;
+// import React from 'react';
+// function Layout({ children, ...props }: React.PropsWithChildren<{}>) {
+//   return (
+//     <div role={'Layout'} {...props}>
+//       {children}
+//     </div>
+//   );
+// }
+// export default Layout;
