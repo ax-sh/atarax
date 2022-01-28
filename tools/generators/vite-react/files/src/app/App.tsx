ef51161e6aaaa1427b7767175ba9b6344b3ab9b5
Axm <56112475+ax-sh@users.noreply.github.com>
2022-01-28 19:06:55 +0545

    add error state component

--- tools/generators/vite-react/files/src/app/App.tsx
+++ tools/generators/vite-react/files/src/app/App.tsx
@@ -3,9 +3,10 @@ import { getUserInfo } from './apis';
 import Layout from './components/Layout';
 import { useQuery } from 'react-query';
 function App() {
-  const { data, isLoading } = useQuery('userInfo', getUserInfo);
+  const { data, isLoading, error } = useQuery('userInfo', getUserInfo);
 
   if (isLoading) return <h1>Loading</h1>;
+  if (error) return <h1>Error {JSON.stringify(error, null, 4)}</h1>;
   console.log('user info', data);
 
   return (
