0c2df1328fad58626f045ec46a7d48e74f800642
Axm <56112475+ax-sh@users.noreply.github.com>
2022-01-28 10:48:56 +0545

    feat: add generator for modified vite app

--- tools/generators/vite-react/index.ts
+++ tools/generators/vite-react/index.ts
@@ -1,8 +1,39 @@
-import { Tree, formatFiles, installPackagesTask } from '@nrwl/devkit';
-import { libraryGenerator } from '@nrwl/workspace/generators';
+import {
+  Tree,
+  formatFiles,
+  installPackagesTask,
+  generateFiles,
+  readProjectConfiguration,
+  updateProjectConfiguration,
+} from '@nrwl/devkit';
+import { Linter } from '@nrwl/linter';
+
+import { applicationGenerator } from '@nxext/react/src/generators/application/application';
+import * as path from 'path';
 
 export default async function (tree: Tree, schema: any) {
-  await libraryGenerator(tree, { name: schema.name });
+  await applicationGenerator(tree, {
+    name: schema.name,
+    style: 'scss',
+    skipFormat: false,
+    unitTestRunner: 'none',
+    linter: Linter.EsLint,
+    pascalCaseFiles: true,
+    globalCss: true,
+    babelJest: false,
+  });
+  const project = await readProjectConfiguration(tree, schema.name);
+  project.targets = project.targets || {};
+  const configFile = path.join('apps/', schema.name, 'vite.config.ts');
+  project.targets.serve.options.configFile = configFile;
+  updateProjectConfiguration(tree, schema.name, project);
+
+  await generateFiles(
+    tree,
+    path.join(__dirname, 'files'),
+    path.join('apps/', schema.name),
+    schema
+  );
   await formatFiles(tree);
   return () => {
     installPackagesTask(tree);
