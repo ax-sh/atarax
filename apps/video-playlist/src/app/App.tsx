1349f39086ce6848cb43d3de6aebae3b086dbc1c
Axm <56112475+ax-sh@users.noreply.github.com>
2022-01-28 14:26:56 +0545

    add video wrapper

--- apps/video-playlist/src/app/App.tsx
+++ apps/video-playlist/src/app/App.tsx
@@ -1,9 +1,38 @@
+import React from 'react';
 import Layout from './components/Layout';
+
+const Video = ({ src }: { src: string }) => {
+  return (
+    <div className="video-wrapper h-full w-full flex justify-center aspect-video ">
+      <video
+        className="block h-full object-contain object-center"
+        controls
+        src={src}
+      />
+    </div>
+  );
+};
+const VideoAndPlaylist = () => {
+  const [src, setSrc] = React.useState(
+    'http://techslides.com/demos/sample-videos/small.mp4'
+  );
+
+  return (
+    <section className="h-120 bg-red-50 md:grid grid-cols-12">
+      <div className="col-span-8 bg-black relative">
+        <Video src={src} />
+      </div>
+      <div>video</div>
+    </section>
+  );
+};
 
 function App() {
   return (
     <Layout>
-      <header>App</header>
+      <header>
+        <VideoAndPlaylist />
+      </header>
     </Layout>
   );
 }
