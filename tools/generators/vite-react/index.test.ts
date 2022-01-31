32dca068d93e2448f78f640fb653be5e81b7da52
Axm <56112475+ax-sh@users.noreply.github.com>
2022-01-31 10:36:12 +0545

    add devkit imports

--- tools/generators/vite-react/index.test.ts
+++ tools/generators/vite-react/index.test.ts
@@ -1,4 +1,32 @@
+import {
+  Tree,
+  addProjectConfiguration,
+  readProjectConfiguration,
+} from '@nrwl/devkit';
+import { createTreeWithEmptyWorkspace } from '@nrwl/devkit/testing';
+// import generator from "./config.impl";
+
 describe('vite-react', () => {
+  const projectName = 'vite-react';
+  let host: Tree;
+
+  beforeEach(() => {
+    host = createTreeWithEmptyWorkspace();
+
+    addProjectConfiguration(host, projectName, {
+      projectType: 'application',
+      root: `./apps/${projectName}`,
+      sourceRoot: `./apps/${projectName}/src`,
+      targets: {
+        build: {
+          executor: '@test/test:build',
+          options: {
+            test: 'test',
+          },
+        },
+      },
+    });
+  });
   it.only('sanity test', () => {
     expect(1 + 1).toBe(2);
   });
