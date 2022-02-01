fcdc717b957f50d0d427d102946bae6c81f75c09
Axm <56112475+ax-sh@users.noreply.github.com>
2022-02-02 00:30:16 +0545

    revert

--- tools/generators/vite-react/files/vite.config.ts
+++ tools/generators/vite-react/files/vite.config.ts
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite';
 // import react from '@vitejs/plugin-react';
 import WindiCSS from 'vite-plugin-windicss';
-import VitePluginHtmlEnv from 'vite-plugin-html-env';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [WindiCSS(), VitePluginHtmlEnv()],
+	plugins: [WindiCSS()],
 	// plugins: [react(), WindiCSS()],
 	server: { host: true },
 });
