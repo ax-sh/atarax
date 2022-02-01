bdd27f677f8b7dcb2b5207d193f17533c152884c
Axm <56112475+ax-sh@users.noreply.github.com>
2022-02-01 20:01:54 +0545

    update vite config

--- tools/generators/vite-react/files/vite.config.ts
+++ tools/generators/vite-react/files/vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+// import react from '@vitejs/plugin-react';
 import WindiCSS from 'vite-plugin-windicss';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react(), WindiCSS()],
+	plugins: [WindiCSS()],
+	// plugins: [react(), WindiCSS()],
 	server: { host: true },
 });
