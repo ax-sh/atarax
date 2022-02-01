1f0de8cdcf6fc89e46126a219c5e6d6585bbdda4
Axm <56112475+ax-sh@users.noreply.github.com>
2022-02-01 19:25:31 +0545

    add axios

--- tools/generators/vite-react/files/src/app/apis/index.ts
+++ tools/generators/vite-react/files/src/app/apis/index.ts
@@ -1,5 +1,16 @@
+import axios from 'axios';
+
+const baseURL = 'https://anime-facts-rest-api.herokuapp.com/api/v1';
+export const instance = axios.create({
+	baseURL,
+	timeout: 1000,
+	headers: { 'X-Custom-Header': 'foobar' },
+});
 function getUserInfo() {
-  return fetch('/api/user/info').then((res) => res.json());
+	return instance.get('/api/user/info').then((res) => res.data);
+}
+function getAnimeInfo() {
+	return instance.get('/fma_brotherhood/2').then((res) => res.data);
 }
 
-export { getUserInfo };
+export { getUserInfo, getAnimeInfo };
