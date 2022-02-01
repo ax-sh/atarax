3994311ff47b4587bbdbe8c76d44de938709c406
Axm <56112475+ax-sh@users.noreply.github.com>
2022-02-01 19:33:21 +0545

    update vite config

--- apps/react-auth-app/vite.config.ts
+++ apps/react-auth-app/vite.config.ts
@@ -1,9 +1,12 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+// import react from '@vitejs/plugin-react';
 import WindiCSS from 'vite-plugin-windicss';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react(), WindiCSS()],
+	plugins: [
+		// react(),
+		WindiCSS(),
+	],
 	server: { host: true },
 });
